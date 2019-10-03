0fb3b0017598b90046d2d175730ba470733add17
SUITE <SUITE@SUITEC103-ST15.cibertec.edu.pe>
2019-10-02 20:58:10 -0500

    Mensaje 1

---
+++ Prueba/src/Pb/Eq.java
@@ -0,0 +1,19 @@
+package Pb;
+
+import java.util.Scanner;
+
+public class Eq {
+	
+	private void mai() {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String name ;
+		
+		System.out.println(" Digite su nombre para confirmar asistencia : ");  
+		name = sc.nextLine();				
+				
+		System.out.println("Hola "+name + " su asistencia fue registrada");
+	}
+
+}
